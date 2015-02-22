ca2a4b19fc0db97b2c121dad6ff553ef9f149575
Carolinatxs <carolinatxs2@gmail.com>
2015-02-21 23:26:47 -0500

    Revert "The inverse of the a Matrix"
    This reverts commit b54489bfcc95ec09b9214c4c571ca654be2a531d.

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -9,14 +9,3 @@ makeCacheMatrix <- function(x = matrix()) {
     getinverse <- function() inv
     list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-cacheSolve <- function(x, ...) {
-    inv <- x$getinverse()
-    if(!is.null(inv)) {
-        message("getting cached data.")
-        return(inv)
-    }
-    data <- x$get()
-    inv <- solve(data)
-    x$setinverse(inv)
-    inv
-}
